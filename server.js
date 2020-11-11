15f531175ea1eb18ac987406f83e3e9a2013c1f8
yusei <yusei5884@gmail.com>
2020-11-12 00:00:09 +0900

    modify dbURL

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import User from './user'
 
 const app = express()
 const port = process.env.PORT || 3001
-const dbUrl = 'mongodb://heroku_11gs2dg8:so3b32tq5cebib5u6b02g5ttio@ds033186.mlab.com:33186/heroku_11gs2dg8'
+const dbUrl = process.env.DB_URI
 
 
 app.use(express.static(path.join(__dirname, 'client/build')))
